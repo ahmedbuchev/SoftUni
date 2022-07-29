46fe9efca65f239e1af8086589dc8ccdb6903ca9
ahmedbuchev <buchev@gmail.com>
2022-07-29 07:37:20 +0300

    Added Exam Preparation

--- 01.Fundamentals/10.regex/exercise/extract_emails.py
+++ 01.Fundamentals/10.regex/exercise/extract_emails.py
@@ -5,3 +5,11 @@ pattern = r"(^|(?<=\s))[a-zA-Z0-9]+[\._-]?[a-zA-Z0-9]+@[a-z]+-?[a-z]+(\.[a-z]+)+
 
 emails = [el.group() for el in re.finditer(pattern, text)]
 print(*emails, sep="\n")
+
+# Ivan Shopov
+#
+# some_string = input()
+# pattern = r'(?<=\s)(([a-z0-9]+[\.\-\_a-z0-9]*)@[a-z\-]+(\.[a-z]+)+)\b'
+# matches = re.findall(pattern, some_string)
+# for match in matches:
+#     print(match[0])
--- 01.Fundamentals/10.regex/exercise/furniture.py
+++ 01.Fundamentals/10.regex/exercise/furniture.py
@@ -17,3 +17,19 @@ for furniture in furniture_name:
     print(furniture)
 print(f"Total money spend: {total_money:.2f}")
 
+# Ivan Shopov
+# pattern = r'>>([A-Za-z]+)<<(\d+\.?\d*)!(\d+)'
+# total_cost = 0
+# bought_furniture = []
+# command = input()
+# while command != "Purchase":
+#     match = re.search(pattern, command)
+#     if match:
+#         furniture, price , quantity = match.groups()
+#         bought_furniture.append(furniture)
+#         total_cost += int(quantity) * float(price)
+#     command = input()
+# print("Bought furniture:")
+# for furniture in bought_furniture:
+#     print(furniture)
+# print(f"Total money spend: {total_cost:.2f}")
